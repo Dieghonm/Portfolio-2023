2ef81c8e232a9bedb426bb44aee0e3129b1b8c39
Diegho <dieghonm@gmail.com>
2023-05-25 17:22:43 -0300

    Altera o texto do componente About

--- src/componentes/About.jsx
+++ src/componentes/About.jsx
@@ -15,11 +15,24 @@ function About() {
         <span className="about-content">
           <div>
         <h5 className="about-subtitle">Um pouco sobre mim</h5>
-            <p>Desenvolvedor web movido pela curiosidade e prazer em criar coisas que vivem na internet.</p>
-            <p>Meu interesse em desenvolvimento começou em 2000 quando um grupo de amigos decidiu criar <i>mod's</i> para um jogo online. Na época, não era tão simples encontrar materiais sobre programação, e mesmo tendo aprendido algumas coisas, minha vida me levou por outros caminhos.</p>
-            <p>No final de 2007, me formei em administração de empresas e trabalhei com isso em uma empresa pública até 2019, quando por oportunidade da pandemia eu resolvi finalmente estudar programação e me inscrevi na Trybe, onde aprendi os fundamentos que precisava para seguir com as minhas próprias pernas.</p>
-            <p>Em 2022, tive o privilégio de trabalhar em duas <i>start-ups</i>, uma como desenvolvedor <i>front-end</i> e na outra como instrutor de <i>front-end</i>.</p>
-            <p>Sou apaixonado por resolver desafios lógicos e dar vida a ideias e projetos de produtos digitais. Atualmente, me dedico a ampliar meus conhecimentos com <i>back-end</i> e com outras linguagens como o <i>Python</i>.</p>
+            {/* <p>Desenvolvedor web movido pela curiosidade e prazer em criar coisas que vivem na internet.</p>
+
+            <p>Meu interesse pelo desenvolvimento começou em 2000, quando um grupo de amigos decidiu criar modificações para um jogo online. Naquela época, encontrar materiais sobre programação não era tão simples, mas mesmo assim, aprendi algumas coisas. No entanto, minha trajetória profissional acabou me levando por outros caminhos.</p>
+
+            <p>Após concluir minha graduação em Administração de Empresas no final de 2007, trabalhei nessa área em uma empresa pública até 2019. Foi durante a pandemia que surgiu a oportunidade de finalmente seguir minha paixão pela programação e adquirir os fundamentos necessários para trilhar meu próprio caminho como desenvolvedor.</p>
+
+            <p>Em 2022, tive o privilégio de trabalhar em duas <i>start-ups</i>, uma como desenvolvedor e na outra como instrutor de <i>front-end</i>.Essas experiências me proporcionaram a oportunidade de aprimorar minhas habilidades técnicas e também de compartilhar meus conhecimentos com outras pessoas.</p>
+
+            <p>Sou apaixonado por resolver desafios lógicos e dar vida a ideias e projetos de produtos digitais. Estou constantemente buscando expandir meus conhecimentos em áreas como <i>back-end</i> e linguagens adicionais, como o <i>Python</i>.</p>
+            
+            <p>Estou sempre em busca de novas oportunidades para colaborar em projetos emocionantes e desafiadores. Se você procura um desenvolvedor dedicado, criativo e comprometido em entregar soluções digitais de alta qualidade, ficarei feliz em fazer parte da sua equipe.</p> */}
+            
+            <p>Sou um desenvolvedor web apaixonado por criar soluções digitais que ganham vida na internet. Minha jornada no desenvolvimento começou em 2000, quando mergulhei no estudo de programação para criar modificações em um jogo online. Ao longo do tempo, enfrentei desafios e minha carreira tomou diferentes direções. No entanto, a pandemia me proporcionou uma oportunidade única de dedicar-me à programação e seguir minha paixão.</p>
+
+            <p>Durante o ano de 2022, tive o privilégio de trabalhar em duas <i>startups</i>, onde aprimorei minhas habilidades como desenvolvedor. Trabalhar nesses ambientes dinâmicos e inovadores me permitiu enfrentar desafios lógicos e transformar ideias em produtos digitais reais. Estou constantemente buscando ampliar meus conhecimentos, concentrando-me atualmente no desenvolvimento <i>back-end</i> e explorando linguagens como <i>Python</i>.</p>
+
+            <p>Ao longo da minha jornada, conquistei resultados significativos e desenvolvi um conjunto sólido de habilidades técnicas. Minha paixão pela programação é impulsionada pela busca constante por projetos emocionantes nos quais possa contribuir de forma significativa. Tenho um compromisso inabalável em entregar soluções digitais de alta qualidade, e estou pronto para colaborar com equipes que compartilhem dessa visão.</p>
+
           </div>
         </span>
       </div>
